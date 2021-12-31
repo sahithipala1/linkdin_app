92831760ae4827175c83c2afaf155e354085d3ab
sahithipala1 <sahithipala1@gmail.com>
2021-12-30 22:02:29 -0500

    lists

---
+++ add.py
@@ -0,0 +1,19 @@
+nums = [2, 7, 11, 15]
+target = 26
+
+# Brute force solution
+for outer_index in range(len(nums)):
+    for inner_index in range(outer_index+1, len(nums)):
+        if nums[outer_index] + nums[inner_index] == target:
+            print(outer_index, inner_index)
+
+
+# While loop solution
+for outer_index in range(len(nums)):
+    inner_index = outer_index + 1
+    while inner_index < len(nums):
+        print(outer_index, inner_index)
+        if nums[outer_index] + nums[inner_index] == target:
+            print("Answer", outer_index, inner_index)
+
+        inner_index = inner_index + 1
---
+++ learning_list.py
@@ -0,0 +1,30 @@
+input_list = [5, 6, 7, 8]
+target = 11
+
+for i in range(len(input_list)):
+    print(i, input_list[i])
+
+for i in range(4):
+    print(i)
+# If you want to create a by giving range:
+print(list(range(0, 20, 5)))
+
+# If you want to print the values in a list:
+a = [10, 20, 30]
+for i in range(len(a)):
+    print(i, a[i])
+
+print(list(range(2, 5)))
+
+# If we give list it will print the values in list
+z = [10, 30, 50]
+
+for i in range(len(z)):
+    print(i, z[i])
+
+f = range(5, 20, 10)
+
+for k in f:
+    print(k)
+
+
